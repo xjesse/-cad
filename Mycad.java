eb2b33cc88979fb8bc5d916ba7fcf4f70803d566
MC 子龙 <x-jesse@foxmail.com>
2017-02-18 20:13:05 +0800

    文件简单修改

--- Mycad.java
+++ Mycad.java
@@ -1,14 +1,14 @@
 package CAD;
 
 public class Mycad {
-	
 
 	public static void main(String[] args) {
+
 		@SuppressWarnings("unused")
 		window wid = new window();
-
 		System.out.println("zhix");
 	
+
 	}
 
 
