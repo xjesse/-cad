67dcc01370e7fcab95300355c7c3b2c8ad2146c2
MC 子龙 <x-jesse@foxmail.com>
2017-02-18 20:04:12 +0800

    提交java文件

---
+++ Mycad.java
@@ -0,0 +1,15 @@
+package CAD;
+
+public class Mycad {
+	
+
+	public static void main(String[] args) {
+		@SuppressWarnings("unused")
+		window wid = new window();
+
+		System.out.println("zhix");
+	
+	}
+
+
+}
